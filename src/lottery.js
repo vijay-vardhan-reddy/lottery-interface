c5a4e82cff4613eccbaf86f775f14d93b93fa5d1
vijay-vardhan-reddy <112805442+vijay-vardhan-reddy@users.noreply.github.com>
2022-12-05 20:08:09 +0530

    Add files via upload

---
+++ src/lottery.js
@@ -0,0 +1,4 @@
+import web3 from './web3'
+const address='0x49E083d90085B513Df61F226aC495ad3a9d46Acc'
+const abi= [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+export default new web3.eth.Contract(abi,address);
